5c9a6d28ed1e8cb1d1997e2c69e6ef4f69ef2200
Amrit <AmritMaan2001@gmail.com>
2022-11-19 18:03:37 -0500

    login, register, navbar and footer design complete

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer>
+            <div id="FooterLogo">
+                <div className="FooterLogoText">Blog Inc</div>
+                <div className="FooterLogoAuther">By Amrit</div>
+            </div>
+            <span>
+                Copyright ©2022 All rights reserved
+            </span>
+        </footer>
+    );
 }
 
 export default Footer
